9a3c0dc561b5b837682e64d1e826aa3955ec8a81
G6EJD <d.l.bird@sky.com>
2024-12-17 18:02:15 +0000

    Update ESP32_METAR_Reader_E-Paper_v1.0.ino

--- ESP32_METAR_Reader_E-Paper_v1.0.ino
+++ ESP32_METAR_Reader_E-Paper_v1.0.ino
@@ -762,7 +762,6 @@ void StartWiFi() {
   WiFi.disconnect();
   WiFi.mode(WIFI_STA); // switch off AP
   WiFi.setAutoConnect(true);
-  WiFi.setAutoReconnect(true);
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.printf("STA : Failed!\n");
